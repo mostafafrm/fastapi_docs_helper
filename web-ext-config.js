a73a06b74efc4e1013796ab927c9fe4027d843c5
Sepehr Shirkhanlu <sepehr.shirkhanlou@yahoo.com>
2022-11-10 04:33:58 +0330

    Default configs for web-ext now inherits from "web-ext.env"

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,11 +1,13 @@
+require("dotenv").config({ path: "web-ext.env" });
+const env_vars = process.env;
+
 /*  web-ext environment variables
     for details see: https://extensionworkshop.com/documentation/develop/web-ext-command-reference/
 */
 module.exports = {
-  sourceDir: "./src/",
-  artifactsDir: "./build/",
-
+  sourceDir: env_vars.WEB_EXT_SOURCE_DIR,
+  artifactsDir: env_vars.WEB_EXT_ARTIFACTS_DIR,
   build: {
-    overwriteDest: true,
+    overwriteDest: env_vars.WEB_EXT_OVERWRITE_DEST === "true" ? true : false,
   },
 };
